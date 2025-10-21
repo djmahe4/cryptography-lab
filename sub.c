b80ef6efda86cea49d91f01f81c2998e01aef043
djmahe4 <maheshwaranup@gmail.com>
2025-10-21 19:48:36 +0530

    Removed unnecessary modulus

--- sub.c
+++ sub.c
@@ -7,7 +7,7 @@ void encrypt(char* plaintxt,int key,char* ciphertxt){
     for(i=0;plaintxt[i]!='\0' && plaintxt[i]!='\n';i++){
         if (isalpha(plaintxt[i])){
             char base=isupper(plaintxt[i]) ? 'A':'a';
-            int index=(plaintxt[i]-base+key)%26;
+            int index=plaintxt[i]-base+key;
             ciphertxt[i]=base+index;
         }
         else{
@@ -22,7 +22,7 @@ void decrypt(char* ciphertxt,int key,char* decodedtxt){
     for(i=0;ciphertxt[i]!='\0' && ciphertxt[i]!='\n';i++){
         if (isalpha(ciphertxt[i])){
             char base=isupper(ciphertxt[i]) ? 'A':'a';
-            int index=(ciphertxt[i]-base-key+26)%26;
+            int index=ciphertxt[i]-base-key+26;
             decodedtxt[i]=base+index;
         }
         else{
